279874681b72ff2e1c430dc5d12ccd6f98cbace3
Shariarhosain <shariarhosain131529@gmail.com>
2025-06-15 12:42:20 +0600

    cors fix

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ let criticalConfigMissing = false;
 
 //allow all origins for CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:58266');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
