7f242923f46ddd3fff9556453e05fce9c3df8773
Shariarhosain <shariarhosain131529@gmail.com>
2025-06-15 10:14:09 +0600

    fix jwt

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 
-// This function would typically be called during login
+
 const generateToken = (uid) => {
   return jwt.sign({ uid }, process.env.SECRET_CODE, {
     expiresIn: '1d', // Token expires in 1 day
