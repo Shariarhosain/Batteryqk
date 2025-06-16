1d5567bcccf93dc7e1d07da28ee407fa08b9453d
Shariarhosain <shariarhosain131529@gmail.com>
2025-06-16 12:12:22 +0600

    fix listing

--- src/services/listingService.js
+++ src/services/listingService.js
@@ -399,7 +399,7 @@ uploadImageFromClient: async function (files) {
         }
 
         const response = await axios.post(
-            'https://img.batteryqk.com/upload',
+            'http://q0c040w8s4gcc40kso48cog0.147.93.111.102.sslip.io/upload',
             form,
             {
                 headers: form.getHeaders(),
